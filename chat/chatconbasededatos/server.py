1660a6ae33d604e058280febdc528ec080a551f5
Edwardvee <lautaroivan.quiroga@gmail.com>
2024-05-24 21:23:19 -0300

    chat con inicio de sesion broadcast funcionando

--- chat/chatconbasededatos/server.py
+++ chat/chatconbasededatos/server.py
@@ -1,65 +1,81 @@
 import socket as s
 import threading as t
+import mysql
+import time
+from sqlfunctions import Database
+#import _mysql_connector
 import sys
-PORT = 64444
-HOST = "127.0.0.1"
+clientes = []
+usuarios = []
+server = ("127.0.0.1", 64444)
+socket = s.socket(s.AF_INET, s.SOCK_STREAM)
+socket.bind(server)
+socket.listen(5)
+        
 
-conexiones = []
+print("Escuchando...")
 
-class Client(t.Thread):
-    def __init__(self, socket, address, id, name, signal):
-        t.Thread.__init__(self)
-        self.socket = socket
-        self.address = address
-        self.id = id
-        self.name = name
-        self.signal = signal
 
-        def run(self):
-            print("1")
-            while self.signal:
-                print("2")
-                try: 
-                    print("3")
-                    data = self.socket.recv(32)
-                except: 
-                    print("El cliente " + str(self.address) + " se desconecto.")
-                    self.signal = False
-                    conexiones.remove(self)
-                if data != "":
-                    print("4")
-                    print("Se recibio algo de " + self.address)         
-                    print("ID:" + str(self.id) + ": " + str(data.decode("utf-8")))
-                    for clientes in conexiones:
-                        print("5")
-                        print("hola")
-                        if clientes.id != self.id:
-                            clientes.socket.sendall(data)
+def handle(client):
+    while True: 
+        try:
+            message = client.recv(1024)
+            broadcast(message)
+        except:
+            index = clientes.index(client)
+            clientes.remove(client)
+            client.close()
+            username = usuarios[index]
+            broadcast('{} se ha desconectado!'.format(username).encode('ascii'))
+            usuarios.remove(username)
+            break
 
-def newConnections(socket):
-        while True:
-            sock, address = socket.accept()
-            print("Nueva conexion")
-            print(sock)
-            print(address)
-            client = Client(sock, address, len(conexiones), "Nombre", True)
-            client_thr = t.Thread(target=client.run)
-            client_thr.start()
-            conexiones.append(client)
-            
-            
+def broadcast(msg):
+     for client in clientes:
+          client.send(msg)
 
-def main():
-        server = ("127.0.0.1", 62332)
-        socket = s.socket(s.AF_INET, s.SOCK_DGRAM)
-        socket.bind(server)
-        socket.listen(5)
-        newConnectionsThread = t.Thread(target=newConnections, args=(s,))
-        newConnectionsThread.start()
-        newConnectionsThread.join()
+def receive():
+    while True:
+        client, addr = socket.accept()
+        print("Conexion con {}".format(str(addr)))
+        selection = client.recv(1024).decode('ascii')
+        if selection == "1":
+            username = client.recv(1024).decode('ascii')
+            password = client.recv(1024).decode('ascii')
+            if Database.LogIn(username,password,Database.cursor) != -1:
+                usuarios.append(username)
+                clientes.append(client)
+                print("Nombre de usuario es {}".format(username))
+                broadcast("{} Se ha unido!".format(username).encode('ascii'))
+                client.send('Conectado al servidor!'.encode('ascii'))
+                 # Start Handling Thread For Client
+                thread = t.Thread(target=handle, args=(client,))
+                thread.start()
+            else:
+                print("Sesion invalida, cerrando conexion")
+                client.send('Usuario no existe! Conectese devuelta y vuelva a intentarlo'.encode('ascii'))
+                client.close()
+        if selection == "2":
+            username = client.recv(1024).decode('ascii')
+            password = client.recv(1024).decode('ascii')
+            if Database.SignIn(username,password,Database.cursor, Database.db) != None:
+                usuarios.append(username)
+                clientes.append(client)
+                print("Nombre de usuario es {}".format(username))
+                broadcast("{} Se ha unido!".format(username).encode('ascii'))
+                client.send('Conectado al servidor!'.encode('ascii'))
+                 # Start Handling Thread For Client
+                thread = t.Thread(target=handle, args=(client,))
+                thread.start()
+            else:
+                print("Nombre de usuario utilizado, cerrando conexion.")
+                client.send('Nombre de usuario utilizado, cerrando conexion.'.encode('ascii'))
+                time.sleep(5)
+                client.close()
 
-        print("Escuchando...")
 
-main()
+receive()
+
+
 
 
